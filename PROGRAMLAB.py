51fb883c12df52fea74cac9bed3436f3edaef9e3
EITILABWINF <151474855+EITILABWINF@users.noreply.github.com>
2023-12-03 18:39:28 +0100

    Add files via upload

---
+++ PROGRAMLAB.py
@@ -0,0 +1,5 @@
+lista_imion = ["Anna", "Jan", "Maria", "Piotr", "Katarzyna", "Marek", "Zofia", "Tomasz", "Barbara", "Andrzej"]
+
+print("Oto lista 10 imion:")
+for imie in lista_imion:
+    print(imie)
